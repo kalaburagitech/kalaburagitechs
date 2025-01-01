739a924cd38d90fc01362f1708719ec0810c1a80
kalaburagitech <kalaburagitech@gmail.com>
2025-01-01 23:40:17 +0530

    updated contact page

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,76 @@
+"use client";  // Marking this component as a Client Component
+
+import { useState } from "react";
 import NewsLatterBox from "./NewsLatterBox";
 
 const Contact = () => {
+  // State to hold form data
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    whatsapp: "",
+    projectName: "",
+    language: "",
+    description: "",
+  });
+
+  // State to handle loading and success/error messages
+  const [statusMessage, setStatusMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Handle input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Basic form validation (you can extend this based on your needs)
+    if (!formData.username || !formData.email || !formData.description) {
+      setStatusMessage("Please fill in all required fields.");
+      return;
+    }
+
+    setIsLoading(true);
+    setStatusMessage(null);
+
+    try {
+      const response = await fetch("https://email-nny1.onrender.com/api/email-send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setStatusMessage("Message sent successfully!");
+        setFormData({
+          username: "",
+          email: "",
+          phone: "",
+          whatsapp: "",
+          projectName: "",
+          language: "",
+          description: "",
+        });
+      } else {
+        setStatusMessage("Failed to send message.");
+      }
+    } catch (error) {
+      setStatusMessage("Error sending message.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
@@ -17,18 +87,34 @@ const Contact = () => {
                 Our team of experts will get back to you within 24 hours to
                 discuss your project.
               </p>
-              <form>
+
+              {/* Display status message */}
+              {statusMessage && (
+                <div
+                  className={`mb-4 text-center ${
+                    statusMessage.includes("success") ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  {statusMessage}
+                </div>
+              )}
+
+              <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
+                  {/* Input fields */}
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
                       <label
-                        htmlFor="name"
+                        htmlFor="username"
                         className="mb-3 block text-sm font-medium text-dark dark:text-white"
                       >
                         Your Name
                       </label>
                       <input
                         type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
                         placeholder="Enter your name"
                         className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
                       />
@@ -44,7 +130,46 @@ const Contact = () => {
                       </label>
                       <input
                         type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
                         placeholder="Enter your email"
+                        className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="phone"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Phone
+                      </label>
+                      <input
+                        type="text"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        placeholder="Enter your phone"
+                        className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="whatsapp"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your WhatsApp
+                      </label>
+                      <input
+                        type="text"
+                        name="whatsapp"
+                        value={formData.whatsapp}
+                        onChange={handleChange}
+                        placeholder="Enter your WhatsApp"
                         className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
                       />
                     </div>
@@ -52,22 +177,75 @@ const Contact = () => {
                   <div className="w-full px-4">
                     <div className="mb-8">
                       <label
-                        htmlFor="message"
+                        htmlFor="projectName"
                         className="mb-3 block text-sm font-medium text-dark dark:text-white"
                       >
-                        Your Message
+                        Project Name
+                      </label>
+                      <input
+                        type="text"
+                        name="projectName"
+                        value={formData.projectName}
+                        onChange={handleChange}
+                        placeholder="Enter your project name"
+                        className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="language"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Programming Language
+                      </label>
+                      <select
+                        name="language"
+                        value={formData.language}
+                        onChange={handleChange}
+                        className="w-full rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
+                      >
+                        <option value="">Select a language</option>
+                        <option value="C#">C#</option>
+                        <option value="Java">Java</option>
+                        <option value="HTML">HTML</option>
+                        <option value="JavaScript">JavaScript</option>
+                        <option value="Python">Python</option>
+                        <option value="PHP">PHP</option>
+                        <option value=".NET">.NET</option>
+                        <option value="TypeScript">TypeScript</option>
+                        <option value="Flutter">Flutter</option>
+                        <option value="Swift">Swift</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="description"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Project Description
                       </label>
                       <textarea
-                        name="message"
+                        name="description"
                         rows={5}
+                        value={formData.description}
+                        onChange={handleChange}
                         placeholder="Describe your project"
                         className="w-full resize-none rounded-sm border border-[#FF8C00] bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-[#FF8C00] dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-[#FF8C00] dark:focus:shadow-none"
                       ></textarea>
                     </div>
                   </div>
+
                   <div className="w-full px-4">
-                    <button className="rounded-sm bg-[#FF8C00] px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-[#FF8C00]/90 dark:shadow-submit-dark">
-                      Send Message
+                    <button
+                      type="submit"
+                      className="rounded-sm bg-[#FF8C00] px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-[#FF8C00]/90 dark:shadow-submit-dark"
+                      disabled={isLoading}
+                    >
+                      {isLoading ? "Sending..." : "Send Message"}
                     </button>
                   </div>
                 </div>
