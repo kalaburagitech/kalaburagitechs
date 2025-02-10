dcbbbb402d2737b7fc21569fe82a79d52e9a6996
KalaburagiTech <kalaburagitech@gmail.com>
2025-02-10 16:43:33 +0530

    Update our-services.tsx

--- src/components/services/our-services.tsx
+++ src/components/services/our-services.tsx
@@ -17,7 +17,7 @@ const projects: Project[] = [
     description:
       "🚀 Project Highlights: ✅ Adopt & Donate Pets – Find loving homes for animals!",
     category: "Web Application",
-    youtubeId: "9v0zfQaNagKxetcg",
+    youtubeId: "https://youtu.be/mfG5Jl9xBLk?si=9v0zfQaNagKxetcg",
     githubLink: "https://github.com/yourusername/ecommerce-platform",
     demoLink: "https://kalaburagitech.com/servicespage",
   },
